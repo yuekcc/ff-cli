fb631895f42c4609b58bff51ec7627c8678c71bf
yuekcc <yuekcc@qq.com>
2025-02-26 23:43:35 +0800

    使用 win32 api

--- open-win/src/main.zig
+++ open-win/src/main.zig
@@ -2,25 +2,70 @@
 
 const std = @import("std");
 const process = std.process;
+const windows = std.os.windows;
+
+const SHELLEXECUTEINFOW = extern struct {
+    cbSize: u32,
+    fMask: u32,
+    hwnd: ?windows.HWND,
+    lpVerb: ?[*:0]const u16,
+    lpFile: ?[*:0]const u16,
+    lpParameters: ?[*:0]const u16,
+    lpDirectory: ?[*:0]const u16,
+    nShow: i32,
+    hInstApp: ?windows.HINSTANCE,
+    lpIDList: ?*anyopaque,
+    lpClass: ?[*:0]const u16,
+    hkeyClass: ?windows.HKEY,
+    dwHotKey: u32,
+    Anonymous: extern union {
+        hIcon: ?windows.HANDLE,
+        hMonitor: ?windows.HANDLE,
+    },
+    hProcess: ?windows.HANDLE,
+};
+
+pub extern "shell32" fn ShellExecuteExW(
+    pExecInfo: ?*SHELLEXECUTEINFOW,
+) callconv(windows.WINAPI) windows.BOOL;
+
+// 定义常量
+const SEE_MASK_NOCLOSEPROCESS = 0x00000040;
+const SW_SHOW = 5;
 
 const allocator = std.heap.page_allocator;
-
-fn invokeFile(a: std.mem.Allocator, path: []const u8) !void {
-    const script = try std.fmt.allocPrint(a, "Invoke-Item \"{s}\"", .{path});
-
-    const argv = &[_][]const u8{
-        "powershell",
-        "-c",
-        script,
-    };
-    var child = process.Child.init(argv, a);
-    try child.spawn();
-}
 
 pub fn main() !void {
     var args_iter = try std.process.argsWithAllocator(allocator);
     defer args_iter.deinit();
     _ = args_iter.next().?;
     const filepath = args_iter.next().?;
-    try invokeFile(allocator, filepath);
+
+    var sei: SHELLEXECUTEINFOW = .{
+        .cbSize = @sizeOf(SHELLEXECUTEINFOW),
+        .fMask = SEE_MASK_NOCLOSEPROCESS,
+        .hwnd = null,
+        .lpVerb = try std.unicode.utf8ToUtf16LeAllocZ(allocator, "open"),
+        .lpFile = try std.unicode.utf8ToUtf16LeAllocZ(allocator, filepath),
+        .lpParameters = null,
+        .lpDirectory = null,
+        .nShow = SW_SHOW,
+        .hInstApp = null,
+        .lpIDList = null,
+        .lpClass = null,
+        .hkeyClass = null,
+        .dwHotKey = 0,
+        .Anonymous = .{
+            .hMonitor = null,
+        },
+        .hProcess = null,
+    };
+
+    const result = ShellExecuteExW(&sei);
+    if (result == 0) {
+        std.debug.print("ShellExecuteEx failed\n", .{});
+        return;
+    }
+
+    std.debug.print("Notepad launched successfully\n", .{});
 }
