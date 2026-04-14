57bd38e36292fd0001c18d88245ca35a9b4dd741
yuekcc <yuekcc@qq.com>
2026-04-14 23:22:06 +0800

    更新到 zig 0.16.0-dev

--- open-win/build.zig
+++ open-win/build.zig
@@ -4,22 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "open-win",
+    const root_mod = b.addModule("open", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "open",
+        .root_module = root_mod,
+        .use_llvm = true,
+        .use_lld = true,
     });
 
     b.installArtifact(exe);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- open-win/src/main.zig
+++ open-win/src/main.zig
@@ -4,7 +4,8 @@ const std = @import("std");
 const process = std.process;
 const windows = std.os.windows;
 
-const SHELLEXECUTEINFOW = extern struct {
+// SHELLEXECUTEINFOW
+const ShellExecuteInfo = extern struct {
     cbSize: u32,
     fMask: u32,
     hwnd: ?windows.HWND,
@@ -26,8 +27,8 @@ const SHELLEXECUTEINFOW = extern struct {
 };
 
 pub extern "shell32" fn ShellExecuteExW(
-    pExecInfo: ?*SHELLEXECUTEINFOW,
-) callconv(windows.WINAPI) windows.BOOL;
+    pExecInfo: ?*ShellExecuteInfo,
+) callconv(.winapi) windows.BOOL;
 
 // 定义常量
 const SEE_MASK_NOCLOSEPROCESS = 0x00000040;
@@ -35,14 +36,15 @@ const SW_SHOW = 5;
 
 const allocator = std.heap.page_allocator;
 
-pub fn main() !void {
-    var args_iter = try std.process.argsWithAllocator(allocator);
+pub fn main(init: std.process.Init) !void {
+    const arena: std.mem.Allocator = init.arena.allocator();
+    var args_iter = try init.minimal.args.iterateAllocator(arena);
     defer args_iter.deinit();
-    _ = args_iter.next().?;
-    const filepath = args_iter.next().?;
+    _ = args_iter.next() orelse unreachable;
+    const filepath = args_iter.next() orelse unreachable;
 
-    var sei: SHELLEXECUTEINFOW = .{
-        .cbSize = @sizeOf(SHELLEXECUTEINFOW),
+    var sei: ShellExecuteInfo = .{
+        .cbSize = @sizeOf(ShellExecuteInfo),
         .fMask = SEE_MASK_NOCLOSEPROCESS,
         .hwnd = null,
         .lpVerb = try std.unicode.utf8ToUtf16LeAllocZ(allocator, "open"),
@@ -62,10 +64,5 @@ pub fn main() !void {
     };
 
     const result = ShellExecuteExW(&sei);
-    if (result == 0) {
-        std.debug.print("ShellExecuteEx failed\n", .{});
-        return;
-    }
-
-    std.debug.print("Notepad launched successfully\n", .{});
+    process.exit(if (result.toBool())  1 else 0);
 }
